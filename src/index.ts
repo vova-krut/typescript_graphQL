e7588f10d56182551c6046ca47f675e8f2e192d4
Volodymyr <vthehang@gmail.com>
2022-06-21 18:09:30 +0300

    added base server structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
+import { ApolloServer } from "apollo-server-express";
 import dotenv from "dotenv";
 import "reflect-metadata";
 import express from "express";
+import { buildSchema } from "type-graphql";
+import cookieParser from "cookie-parser";
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground,
+    ApolloServerPluginLandingPageProductionDefault,
+} from "apollo-server-core";
 
 dotenv.config();
 
-async function bootstrap() {}
+const PORT = process.env.PORT || 5000;
+
+async function bootstrap() {
+    const schema = await buildSchema({
+        resolvers, //TODO
+        authChecker, //TODO
+    });
+    const app = express();
+    app.use(cookieParser());
+    const server = new ApolloServer({
+        schema,
+        context: (ctx) => {
+            console.log(ctx);
+            return ctx;
+        },
+        plugins: [
+            process.env.NODE_ENV === "production"
+                ? ApolloServerPluginLandingPageProductionDefault()
+                : ApolloServerPluginLandingPageGraphQLPlayground(),
+        ],
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+}
 
 bootstrap();
