1638de746a3fa7acf796c622698fcec059dc409f
Volodymyr <vthehang@gmail.com>
2022-06-21 17:50:44 +0300

    init commit

---
+++ config/default.ts
@@ -0,0 +1,3 @@
+export default {
+    dbUri: "mongodb+srv://user:root@cluster0.tm4dmf0.mongodb.net/?retryWrites=true&w=majority",
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import "reflect-metadata";
+import express from "express";
+
+dotenv.config();
+
+async function bootstrap() {}
+
+bootstrap();
